5bd738db135ce91424d2828e4322deaa2a219f1e
galushka.alex <galushka.alex.1998@gmail.com>
2019-03-27 20:14:32 +0200

    first commit

---
+++ Lex/Lex.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lex
+{
+    public enum LexType
+    {
+        ReservedWord = 1,
+        Delimiter,
+        OperationMark,
+        Indentifier,
+        StringConst,
+        IntConst,
+        DoubleConst,
+    }
+    public class Lex
+    {
+        
+        private string[] ReservedWords = 
+        {
+            "for",
+            "while",
+            "do",
+            "if",
+            "else",
+            "write",
+            "read",
+            "fread",
+            "fwrite",
+            "cuts",
+            "int",
+            "double",
+            "bool",
+            "signal",
+            "hz",
+            "convol",
+            "timeof"
+        };
+        private string[] OperationMarks = { "+", "-", "*", "/", "%", "<", ">", "=", "==", "!=", "<=", ">=" };
+        private string[] Delimiters = { "(", ")", "\"", "{", "}", ";", ",", "[","]", " "};
+        private string[] Identifiers = { };
+        private string[] StringConsts = { };
+        private string[] NumberIntConsts = { };
+        private string[] NumberDoubleConsts = { };
+        private Regex IndentifierReg = new Regex(@"^[a-z|A-Z]+\w*$");
+        private Regex DoubleConstReg = new Regex(@"^-?\d+\.\d+$");
+        private Regex IntConstReg = new Regex(@"^-?\d+$");
+        private Regex StringConstReg = new Regex("^\"\\S*\"$");
+
+
+
+
+        ICollection <Lexema> Analys(string[] text)
+        {
+            List<Lexema> collection = new List<Lexema>();
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                for(int j = 0; j < text[i].Length; j++)
+                {
+                    string tmplex = string.Empty + text[j];
+                    while (!text[i][j + 1].ToString().BelongTo(Delimiters) && !text[i][j + 1].ToString().BelongTo(OperationMarks))
+                    {
+                        tmplex += text[i][j + 1];
+                        j++;
+                    }
+                    LexType type;
+                    if (tmplex.BelongTo(ReservedWords)) type = LexType.ReservedWord;
+                    else if (tmplex.BelongTo(Delimiters)) type = LexType.Delimiter;
+                    else if (tmplex.BelongTo(OperationMarks)) type = LexType.OperationMark;
+                    else if (IndentifierReg.IsMatch(tmplex)) type = LexType.Indentifier;
+                    else if (IntConstReg.IsMatch(tmplex)) type = LexType.IntConst;
+                    else if (DoubleConstReg.IsMatch(tmplex)) type = LexType.DoubleConst;
+                    else if (StringConstReg.IsMatch(tmplex)) type = LexType.StringConst;
+                    else throw new Exception("Error in " + i + 1 + " line at " + j + 1 + " position");
+
+                    collection.Add(new Lexema(i + 1, j + 1, type, tmplex));
+
+                }
+            }
+            
+        }
+
+    }
+    
+
+    public struct Lexema
+    {
+        int line;
+        int position;
+        LexType type;
+        string name;
+        public Lexema(int line, int pos, LexType type, string name)
+        {
+            this.line = line;
+            this.position = pos;
+            this.type = type;
+            this.name = name;
+        }
+    }
+}
+
+public static class StringExstensions
+{
+    public static bool BelongTo(this string name, string[] LexemClass)
+    {
+        foreach(var lex in LexemClass)
+        {
+            if (lex == name)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ lexanalys/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace lexanalys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CodeBox = new System.Windows.Forms.RichTextBox();
+            this.ReadCode = new System.Windows.Forms.Button();
+            this.AnalysButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CodeBox
+            // 
+            this.CodeBox.Location = new System.Drawing.Point(28, 91);
+            this.CodeBox.Name = "CodeBox";
+            this.CodeBox.Size = new System.Drawing.Size(836, 547);
+            this.CodeBox.TabIndex = 0;
+            this.CodeBox.Text = "";
+            // 
+            // ReadCode
+            // 
+            this.ReadCode.Location = new System.Drawing.Point(51, 29);
+            this.ReadCode.Name = "ReadCode";
+            this.ReadCode.Size = new System.Drawing.Size(124, 44);
+            this.ReadCode.TabIndex = 1;
+            this.ReadCode.Text = "Считать код с файла";
+            this.ReadCode.UseVisualStyleBackColor = true;
+            // 
+            // AnalysButton
+            // 
+            this.AnalysButton.Location = new System.Drawing.Point(206, 29);
+            this.AnalysButton.Name = "AnalysButton";
+            this.AnalysButton.Size = new System.Drawing.Size(128, 44);
+            this.AnalysButton.TabIndex = 2;
+            this.AnalysButton.Text = "Разобрать";
+            this.AnalysButton.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(427, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 650);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.AnalysButton);
+            this.Controls.Add(this.ReadCode);
+            this.Controls.Add(this.CodeBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox CodeBox;
+        private System.Windows.Forms.Button ReadCode;
+        private System.Windows.Forms.Button AnalysButton;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
