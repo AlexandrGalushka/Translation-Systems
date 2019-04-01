f3da053b647132f3a85614f098335780b2d2598b
galushka.alex <galushka.alex.1998@gmail.com>
2019-04-01 10:01:11 +0300

    no message

--- Lex/Lex.cs
+++ Lex/Lex.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -84,6 +81,7 @@ namespace Lex
 
                 }
             }
+            return new List<Lexema>();
             
         }
 
